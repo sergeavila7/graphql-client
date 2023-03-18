c4df1d50e00193b6c3983e264cb63d4e4a724b24
serge_avila7 <serge_avila7@yahoo.com>
2023-03-17 19:46:16 -0600

    [fix] url

--- config/apollo.js
+++ config/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://graphql-server-production-0399.up.railway.app/',
+  uri: 'http://graphql-server-production-0399.up.railway.app',
 });
 
 const authLink = setContext((_, { headers }) => {
